3692759c3dab246f0c818e5bbeeb3879b6d725dd
Mark Veltzer <mark.veltzer@gmail.com>
2020-06-07 19:46:22 +0300

    new version

--- config/python.py
+++ config/python.py
@@ -20,6 +20,6 @@ requirements3 = install_requires
 
 python_requires = ">=3.5"
 
-extras_require={
-#    ':python_version == "2.7"': ['futures'],  # for python2.7 backport of concurrent.futures
+extras_require = {
+    # ':python_version == "2.7"': ['futures'],  # for python2.7 backport of concurrent.futures
 }
--- scripts/create.py
+++ scripts/create.py
@@ -11,7 +11,7 @@ url = 'https://pypi.python.org/pypi?%3Aaction=list_classifiers'
 r = requests.get(url)
 assert r.status_code == 200
 remove_re = re.compile("[ .,']")
-replace_with_underscore = re.compile("[\-/()+]")
+replace_with_underscore = re.compile("[-/()+]")
 replace_with_double_underscore = re.compile(" :: ")
 replace_hash_with_sharp = re.compile("[#]")
 replace_quote_with_backslash_quote = re.compile("'")
@@ -26,4 +26,7 @@ with open("pyclassifiers/values.py", "w") as file_handle:
         name = replace_hash_with_sharp.sub("sharp", name)
         value = x
         value = replace_quote_with_backslash_quote.sub("\\'", value)
-        file_handle.write("{} = '{}'\n".format(name, value))
+        full_string = "{} = '{}'\n".format(name, value) 
+        if len(full_string) >= 120:
+            full_string = "{} = \\\n    '{}'\n".format(name, value)
+        file_handle.write(full_string)

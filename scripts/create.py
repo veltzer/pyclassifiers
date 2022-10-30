e1a35fbf9536dea9aceb9f3636bd79f58cbe3363
Mark Veltzer <mark.veltzer@gmail.com>
2022-10-30 11:55:57 +0200

    linting

--- scripts/create.py
+++ scripts/create.py
@@ -4,11 +4,11 @@
 create the pyclassifiers main file
 """
 
-import requests
 import re
+import requests
 
 url = 'https://pypi.python.org/pypi?%3Aaction=list_classifiers'
-r = requests.get(url)
+r = requests.get(url, timeout=5)
 assert r.status_code == 200
 remove_re = re.compile("[ .,']")
 replace_with_underscore = re.compile("[-/()+]")
@@ -26,7 +26,7 @@ with open("pyclassifiers/values.py", "w") as file_handle:
         name = replace_hash_with_sharp.sub("sharp", name)
         value = x
         value = replace_quote_with_backslash_quote.sub("\\'", value)
-        full_string = "{} = '{}'\n".format(name, value) 
+        full_string = f"{name} = '{value}'\n"
         if len(full_string) >= 120:
-            full_string = "{} = \\\n    '{}'\n".format(name, value)
+            full_string = f"{name} = \\\n    '{value}'\n"
         file_handle.write(full_string)

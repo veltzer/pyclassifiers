7e1fc3ceb409b13437e979d165ef22c4618ec85b
Mark Veltzer <mark.veltzer@gmail.com>
2023-12-29 18:24:57 +0200

    no commit message given

--- pyclassifiers/static.py
+++ pyclassifiers/static.py
@@ -1,5 +1,5 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.0.11"
+VERSION_STR = "0.0.12"
 DESCRIPTION = "classifiers from pypi"
 APP_NAME = "pyclassifiers"
 LOGGER_NAME = "pyclassifiers"

207c523a907c7428cb7f528b29bd5004899b5c63
Mark Veltzer <mark.veltzer@gmail.com>
2025-08-28 22:55:46 +0300

    no commit message given

--- config/version.py
+++ config/version.py
@@ -1,2 +1,2 @@
 """ version of this software """
-tup = (0, 0, 20)
+tup = (0, 0, 21)
--- scripts/create.py
+++ scripts/create.py
@@ -7,27 +7,27 @@ create the pyclassifiers main file
 import re
 import requests
 
-url = 'https://pypi.python.org/pypi?%3Aaction=list_classifiers'
+url = "https://pypi.python.org/pypi?%3Aaction=list_classifiers"
 r = requests.get(url, timeout=5)
 assert r.status_code == 200
-remove_re = re.compile("[ .,']")
+remove_re = re.compile("[ .,\']")
 replace_with_underscore = re.compile("[-/()+]")
 replace_with_double_underscore = re.compile(" :: ")
 replace_hash_with_sharp = re.compile("[#]")
-replace_quote_with_backslash_quote = re.compile("'")
-with open("pyclassifiers/values.py", "w") as file_handle:
+replace_quote_with_backslash_quote = re.compile("\'")
+with open("src/pyclassifiers/values.py", "w") as file_handle:
     file_handle.write("\"\"\" values file \"\"\"\n");
     for x in r.content.decode().split("\n"):
         if not x:
             continue
         name = x
         name = replace_with_double_underscore.sub("__", name)
-        name = remove_re.sub('', name)
+        name = remove_re.sub("", name)
         name = replace_with_underscore.sub("_", name)
         name = replace_hash_with_sharp.sub("sharp", name)
         value = x
         value = replace_quote_with_backslash_quote.sub("\\'", value)
-        full_string = f"{name} = '{value}'\n"
+        full_string = f"{name} = \"{value}\"\n"
         if len(full_string) >= 120:
-            full_string = f"{name} = \\\n    '{value}'\n"
+            full_string = f"{name} = \\\n    \"{value}\"\n"
         file_handle.write(full_string)

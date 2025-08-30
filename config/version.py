851bea033c900ad2d0de07bd8c668f606d816a16
Mark Veltzer <mark.veltzer@gmail.com>
2025-08-30 06:58:37 +0300

    new version

--- config/version.py
+++ config/version.py
@@ -1,2 +1,2 @@
 """ version of this software """
-tup = (0, 0, 22)
+tup = (0, 0, 23)
--- src/pyclassifiers/static.py
+++ src/pyclassifiers/static.py
@@ -1,5 +1,5 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.0.22"
+VERSION_STR = "0.0.23"
 DESCRIPTION = "classifiers from pypi"
 APP_NAME = "pyclassifiers"
 LOGGER_NAME = "pyclassifiers"
